eb890b1d7c2cf7438dd14f0f3134bbfe91b3dcc4
luis <lahoising78@gmail.com>
2021-09-11 14:26:00 -0400

    initial commit

---
+++ include/ezn_file_dialog.h
@@ -0,0 +1,16 @@
+#ifndef __EZN_FILE_DIALOG_H__
+#define __EZN_FILE_DIALOG_H__
+
+namespace ezn
+{
+
+class FileDialog
+{
+public:
+    FileDialog();
+    ~FileDialog();
+};
+
+}
+
+#endif
---
+++ src/ezn_fd_zenity.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ezn_file_dialog.h>
+
+namespace ezn
+{
+
+FileDialog::FileDialog()
+{
+    printf("hola\n");
+}
+
+FileDialog::~FileDialog()
+{
+
+}
+
+
+}
